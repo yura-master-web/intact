a641dae197b9a1531cbbf170b504eccd54924b42
Yura Komarov <uk@keyba.ru>
2020-04-24 12:30:24 +0300

    Добавлен плагин remodal

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,6 +3,7 @@ import $ from 'jquery';
 import 'owl.carousel';
 import 'jquery-validation';
 import 'jquery-mask-plugin';
+import 'remodal';
 // import 'axios';
 
 $(() => {
@@ -17,3 +18,4 @@ import '../blocks/functions/functions-tabs';
 import '../blocks/limitations/limitations-tabs';
 import '../blocks/trust-us/slider';
 import '../blocks/contacts/contacts';
+import '../blocks/modal/';
