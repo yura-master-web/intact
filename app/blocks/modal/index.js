217e48d5074af046e01e8eb962613c8088c92a88
Yura Komarov <uk@keyba.ru>
2020-04-24 14:54:34 +0300

    спасибо за заявку

--- app/blocks/modal/index.js
+++ app/blocks/modal/index.js
@@ -1,1 +1,18 @@
 console.log('modal');
+window.REMODAL_GLOBALS = {
+	NAMESPACE: 'modal',
+	DEFAULTS: {
+		hashTracking: false
+	}
+};
+
+// Что бы в обход всего открыть модальное окно
+let instModal;
+$(() => {
+	$('[data-remodal-id="modal"]').each((i, el) => {
+		instModal = $(el).remodal();
+
+		// console.log(instModal);
+		instModal.open();
+	});
+});
