425d16277678ab3b3ec6c56f01557d51c6be569d
Yura Komarov <uk@keyba.ru>
2020-05-26 12:51:06 +0300

    [#13256] FrontEnd - Отправка формы, ответ сервера, сообщения

--- app/blocks/modal/index.js
+++ app/blocks/modal/index.js
@@ -1,9 +1,9 @@
 // Что бы в обход всего открыть модальное окно
 
-let instModal;
-$(() => {
-	$('[data-remodal-id="modal"]').each((i, el) => {
-		instModal = $(el).remodal();
-		instModal.open();
-	});
-});
+// let instModal;
+// $(() => {
+// 	$('[data-remodal-id="modal"]').each((i, el) => {
+// 		instModal = $(el).remodal();
+// 		instModal.open();
+// 	});
+// });
