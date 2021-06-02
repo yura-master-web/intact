e285cbf0552365ff9eb130662a61758b80ff709c
Yura Komarov <uk@keyba.ru>
2021-06-02 14:16:25 +0300

    Перевод сайта на локальные ссылки

--- app/blocks/trust-us/slider.js
+++ app/blocks/trust-us/slider.js
@@ -4,28 +4,28 @@ $('.js-slider').owlCarousel({
 	items: 2,
 	responsive: {
 		1200: {
-			items: 6
+			items: 6,
 		},
 		992: {
-			items: 5
+			items: 5,
 		},
 		768: {
-			items: 4
+			items: 4,
 		},
 		576: {
-			items: 3
-		}
+			items: 3,
+		},
 	},
 	navText: [
 		[
 			'<svg class="slider-arrow slider-arrow_prev arrow-style">',
-			'<use xlink:href="/assets/images/icon.svg#icon_arrow"></use>',
-			'</svg>'
+			'<use xlink:href="assets/images/icon.svg#icon_arrow"></use>',
+			'</svg>',
 		].join(''),
 		[
 			'<svg class="slider-arrow slider-arrow_next arrow-style">',
-			'<use xlink:href="/assets/images/icon.svg#icon_arrow"></use>',
-			'</svg>'
-		].join('')
-	]
+			'<use xlink:href="assets/images/icon.svg#icon_arrow"></use>',
+			'</svg>',
+		].join(''),
+	],
 });
